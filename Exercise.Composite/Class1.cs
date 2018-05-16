1201efaa7485b5295624f5ccac8c51d4d2d23469
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-05-16 21:25:48 +0200

    Add project files.

---
+++ Exercise.Composite/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Composite
+{
+    public class Class1
+    {
+    }
+}
