0fcf814765895ef88523e62de5b5a16279f10baf
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-05-17 20:40:02 +0200

    update extensions

--- Exercise.Composite/ExtensionMethods/CompositeChildExtensions.cs
+++ Exercise.Composite/ExtensionMethods/CompositeChildExtensions.cs
@@ -2,7 +2,10 @@
 {
     public static class CompositeChildExtensions
     {
-
+        /// <summary>
+        /// Invokes all composites.Bubble on the way up except the current composite
+        /// </summary>
+        /// <param name="composite"></param>
         public static void InvokeBubbleAllUp(this ICompositeChild composite)
         {
             // InvokeParent
@@ -12,6 +15,22 @@
             if (composite.Parent is ICompositeChild myParentIsChild)
             {
                 myParentIsChild.InvokeBubbleAllUp();
+            }
+        }
+
+        /// <summary>
+        /// Invokes all composites.Bubble on the way up except the current composite
+        /// </summary>
+        /// <param name="composite"></param>
+        public static void InvokeBubbleAllUp<T>(this ICompositeChild<T> composite, T input)
+        {
+            // InvokeParent
+            var output = composite.Parent.Bubble.Invoke(input);
+
+            // if the parent is a child -> start recrusion here
+            if (composite.Parent is ICompositeChild<T> myParentIsChild)
+            {
+                myParentIsChild.InvokeBubbleAllUp(output);
             }
         }
     }
--- Exercise.Composite/ExtensionMethods/CompositeParentExtensions.cs
+++ Exercise.Composite/ExtensionMethods/CompositeParentExtensions.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise.Composite
 {
     public static class CompositeParentExtensions
     {
-        public static void InitChilds(this ICompositeParent composite, Action<ICompositeChild> executeAfterInit = null) 
+        public static void InitChilds(this ICompositeParent composite, Action<ICompositeChild> executeAfterInit = null)
+        {
+            foreach (var child in composite.Childs)
+            {
+                child.Parent = composite;
+
+                executeAfterInit?.Invoke(child);
+            }
+        }
+
+        public static void InitChilds<T>(this ICompositeParent<T> composite, Action<ICompositeChild<T>> executeAfterInit = null)
         {
             foreach (var child in composite.Childs)
             {
@@ -29,6 +37,21 @@ namespace Exercise.Composite
             });
         }
 
+        public static void InitChildsRecrusive<T>(this ICompositeParent<T> composite)
+        {
+            InitChilds(composite, child =>
+            {
+                if (child is ICompositeParent<T> parent)
+                {
+                    InitChildsRecrusive(parent);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Invokes all composites.Bubble on the way down except the current composite
+        /// </summary>
+        /// <param name="composite"></param>
         public static void InvokeBubbleAllDown(this ICompositeParent composite)
         {
             // Invoke all childs
@@ -44,7 +67,31 @@ namespace Exercise.Composite
             }
         }
 
+        /// <summary>
+        /// Invokes all composites.Bubble on the way down except the current composite
+        /// </summary>
+        /// <param name="composite"></param>
+        public static void InvokeBubbleAllDown<T>(this ICompositeParent<T> composite, T input)
+        {
+            // Invoke all childs
+            foreach (var child in composite.Childs)
+            {
+                var output = child.Bubble.Invoke(input);
+
+                // if the child is a parent -> start recrusion here
+                if (child is ICompositeParent<T> myChildIsParent)
+                {
+                    myChildIsParent.InvokeBubbleAllDown(output);
+                }
+            }
+        }
+
         public static IEnumerable<ICompositeParent> ConcatParents(params IEnumerable<ICompositeParent>[] parents)
+        {
+            return parents.Aggregate((a, b) => a.Concat(b));
+        }
+
+        public static IEnumerable<ICompositeParent<T>> ConcatParents<T>(params IEnumerable<ICompositeParent<T>>[] parents)
         {
             return parents.Aggregate((a, b) => a.Concat(b));
         }
