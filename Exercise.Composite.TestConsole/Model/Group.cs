06b89ddc1f4d67d1660537636f14ebba557fc313
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-05-17 06:45:05 +0200

    Add fake model

---
+++ Exercise.Composite.TestConsole/Model/Group.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Composite.TestConsole.Model
+{
+    public class Group : ICompositeParent
+    {
+        public Group()
+        {
+            Bubble = () => Console.WriteLine($"Bubble up -> {nameof(Group)} Name: {Name}");
+        }
+
+        public string Name { get; set; }
+        List<User> Users { get; set; }
+
+        public IEnumerable<ICompositeChild> Childs => Users;
+        public Action Bubble { get; set; }
+    }
+
+    public class User : ICompositeChild, ICompositeParent
+    {
+        public User()
+        {
+            (this as ICompositeChild).Bubble = () => Console.WriteLine($"Bubble up -> {nameof(User)} Name: {Name}");
+            (this as ICompositeParent).Bubble = () => Console.WriteLine($"Bubble down -> {nameof(User)} Name: {Name}");
+        }
+
+        public string Name { get; set; }
+        List<Car> Cars { get; set; }
+
+        public ICompositeParent Parent { get; set; }
+        public IEnumerable<ICompositeChild> Childs => Cars;
+
+        Action ICompositeChild.Bubble { get; set; }
+        Action ICompositeParent.Bubble { get; set; }
+    }
+
+    public class Car : ICompositeChild
+    {
+        public Car()
+        {
+            Bubble = () => Console.WriteLine($"Bubble up -> {nameof(User)} Name: {Color}");
+        }
+
+        public string Color { get; set; }
+
+        public ICompositeParent Parent { get; set; }
+        public Action Bubble { get; set; }
+    }
+}
